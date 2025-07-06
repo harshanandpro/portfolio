a1f90a25550f8347b708ff1c528a57952f1f5eae
Harsh Anand <harshanandpro07@gmail.com>
2025-07-06 15:55:03 +0530

    deply

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -14,13 +14,13 @@ const Skills = () => {
     <div className='skills'>
         <span className="skills_header">Here’s my current tech stack and what I’m confident using.</span>
         <div className="skills_skills">
-            <SkillsCard iconx={ReactIcon} name="React"/>
-            <SkillsCard iconx={PythonIcon} name="React"/>
-            <SkillsCard iconx={NodeIcon} name="React"/>
-            <SkillsCard iconx={JSIcon} name="React"/>
-            <SkillsCard iconx={HtmlIcon} name="React"/>
-            <SkillsCard iconx={CssIcon} name="React"/>
-            <SkillsCard iconx={BTIcon} name="React"/>
+            <SkillsCard iconx={ReactIcon} name="React" shortline="Build dynamic, responsive UIs with React’s component-based magic"/>
+            <SkillsCard iconx={PythonIcon} name="Python" shortline={"Scripts, automation, and data tasks with clean Python code"}/>
+            <SkillsCard iconx={NodeIcon} name="Node Js" shortline="Server-side scripting for scalable web apps and APIs"/>
+            <SkillsCard iconx={JSIcon} name="JS" shortline={"Power interactivity and logic with clean, efficient code"}/>
+            <SkillsCard iconx={HtmlIcon} name="HTML" shortline="Craft clean, semantic markup for solid web foundations"/>
+            <SkillsCard iconx={CssIcon} name="CSS" shortline={"Style it up with modern, responsive, sleek designs"}/>
+            <SkillsCard iconx={BTIcon} name="BootStrap" shortline={"Rapidly design responsive layouts with Bootstrap’s grid"}/>
             
         </div>
     </div>
--- src/SkillsCard.jsx
+++ src/SkillsCard.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
-import './SkillsCard.css'
-const SkillsCard = ({iconx , name , shortline}) => {
+import React from 'react';
+import './SkillsCard.css';
+
+const SkillsCard = ({ iconx, name, shortline }) => {
   return (
     <div className='shine-border'>
-
-    
-    <div className='skillscard'>
-        <img src={iconx} alt={name} width={"100px"} height={"100px"} />
-        <span className='skillscard_text'>{name}</span>
+      <div className='skillscard'>
+        <div className='skillscard-inner'>
+          <div className='skillscard-front'>
+            <img src={iconx} alt={name} width="100px" height="100px" />
+            <span className='skillscard_text'>{name}</span>
+          </div>
+          <div className='skillscard-back'>
+            <p className='skillscard_desc'>{shortline}</p>
+          </div>
+        </div>
+      </div>
     </div>
-    </div>
-  )
-}
-
+  );
+};
 export default SkillsCard
